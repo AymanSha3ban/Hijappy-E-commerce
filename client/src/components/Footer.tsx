409d27a3111315e421fa99cdddf0ae34b04a95c4
Ayman Shaaban <aymankhattap483@gmail.com>
2026-05-07 21:21:00 +0300

    Change logo
    Updated logo image source from 'logo.png' to 'weblogo.png'.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -122,7 +122,7 @@ export default function Footer() {
                         {/* Logo Container - Made it more compact with a larger image focus */}
                     
                             <img 
-                              src="/logo.png" 
+                              src="/weblogo.png" 
                               alt="Louli" 
                               className="w-20 h-20 object-contain transform transition-transform duration-300 group-hover:scale-150" 
                             />
