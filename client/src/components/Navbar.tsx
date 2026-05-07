f8a7bc57923d7cce668d3ddedb1ae8b594c43183
Ayman Shaaban <aymankhattap483@gmail.com>
2026-05-07 21:21:33 +0300

    Change logo image source in Navbar
    Updated logo source from 'logo.png' to 'weblogo.png' in Navbar component.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -97,7 +97,7 @@ export default function Navbar() {
           {/* Logo Container - Made it more compact with a larger image focus */}
       
               <img 
-                src="/logo.png" 
+                src="/weblogo.png" 
                 alt="Louli" 
                 className="w-20 h-20 object-contain transform transition-transform duration-300 group-hover:scale-150" 
               />
